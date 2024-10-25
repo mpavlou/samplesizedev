70c9723e06b7c9d088c0c0403f59a62d9c30d7d3
mpavlou <m.pavlou@ucl.ac.uk>
2024-10-25 18:43:49 +0100

    d

--- R/samplesizedev_binary_s.R
+++ R/samplesizedev_binary_s.R
@@ -47,10 +47,16 @@ samplesizedev_binary_s <- function(S, p, c,   n.predictors, beta = rep(1/n.predi
 
   n_init <- round((n.predictors)/ ((S-1)*log(1-r2/S)))
 
-  if (c<=0.7)            {inflation_f   <- 1.1 ; min.opt  <- n_init*0.35}
-  if (c>0.7  & c<=0.8)   {inflation_f   <- 1.5  ; min.opt <- n_init*0.7}
-  if (c>0.8  & c<=0.85)  {inflation_f   <- 2.1    ; min.opt <- n_init}
-  if (c>0.85 & c<=0.9)   {inflation_f   <- 2.8  ; min.opt <- n_init}
+  if (c<=0.7  )               {inflation_f   <- 1.1 ; min.opt  <- n_init*0.4}
+  if (c>0.7  & c<=0.8 )       {inflation_f   <- 1.5  ; min.opt <- n_init*0.7}
+  if (c>0.8  & c<=0.85)       {inflation_f   <- 2.1    ; min.opt <- n_init}
+  if (c>0.85 & c<=0.9)        {inflation_f   <- 2.8  ; min.opt <- n_init}
+
+
+  if (c<=0.7 & p<0.05 & n.predictors <5)            {inflation_f   <- 0.9 ; min.opt  <- n_init*0.3}
+  if (c>0.7  & c<=0.8 & p<0.05 & n.predictors <5)   {inflation_f   <- 1  ; min.opt <- n_init*0.4}
+  if (c>0.8  & c<=0.85)                             {inflation_f   <- 2.1    ; min.opt <- n_init}
+  if (c>0.85 & c<=0.9)                              {inflation_f   <- 2.8  ; min.opt <- n_init}
 
   max.opt <- inflation_f * n_init
 
