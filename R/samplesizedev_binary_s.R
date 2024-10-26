aff61292f5a5787facd572185852b04af1b483ad
mpavlou <m.pavlou@ucl.ac.uk>
2024-10-26 10:03:26 +0100

    g

--- R/samplesizedev_binary_s.R
+++ R/samplesizedev_binary_s.R
@@ -53,10 +53,10 @@ samplesizedev_binary_s <- function(S, p, c,   n.predictors, beta = rep(1/n.predi
   if (c>0.85 & c<=0.9)        {inflation_f   <- 2.8  ; min.opt <- n_init}
 
 
-  if (c<=0.7 & p<0.05 & n.predictors <5)            {inflation_f   <- 0.9 ; min.opt  <- n_init*0.3}
-  if (c>0.7  & c<=0.8 & p<0.05 & n.predictors <5)   {inflation_f   <- 1  ; min.opt <- n_init*0.4}
-  if (c>0.8  & c<=0.85 & p<0.05 & n.predictors <5)  {inflation_f   <- 2.1    ; min.opt <- n_init *0.5}
-  if (c>0.85 & c<=0.9 & p<0.05 & n.predictors <5)   {inflation_f   <- 2.8  ; min.opt <- n_init*0.7}
+  if (c<=0.7  & n.predictors <5)            {inflation_f   <- 0.9 ; min.opt  <- n_init*0.3}
+  if (c>0.7  & c<=0.8  & n.predictors <5)   {inflation_f   <- 1  ; min.opt <- n_init*0.4}
+  if (c>0.8  & c<=0.85 & n.predictors <5)  {inflation_f   <- 2.1    ; min.opt <- n_init *0.5}
+  if (c>0.85 & c<=0.9  & n.predictors <5)   {inflation_f   <- 2.8  ; min.opt <- n_init*0.7}
 
   max.opt <- inflation_f * n_init
 
